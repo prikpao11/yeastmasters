7ae70bac17d9a4d94e8794ab51f3d1122afd663d
prikpao11 <138899011+prikpao11@users.noreply.github.com>
2025-04-05 04:41:13 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $username = "meld";
+    $password = "123456";
 
     try {
         $conn = new PDO("mysql:host=$servername;dbname=yeast", $username, $password);
